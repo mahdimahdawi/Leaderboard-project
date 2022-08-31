7028102521420edfba6d5d9165f0510e78b75de4
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-08-31 11:11:00 +0430

    fix Linter Error

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
-import _, { forEach } from 'lodash';
 import './style.css';
 
-class Score{
-  constructor(name, score){
+class Score {
+  constructor(name, score) {
     this.name = name;
     this.score = score;
   }
 }
 
+const DispalyScore = () => {
+  const existScore = JSON.parse(localStorage.getItem('scores'));
+  if (existScore !== null && existScore.length > 0) {
+    const scoreList = document.querySelector('.score-list');
+    const scoreItem = document.createElement('ul');
+    scoreList.innerHTML = '';
+
+    existScore.forEach((score) => {
+      const scoreListItem = document.createElement('li');
+      scoreListItem.innerHTML = `${score.name}  :  ${score.score}`;
+      scoreItem.appendChild(scoreListItem);
+      scoreList.appendChild(scoreItem);
+    });
+  }
+};
+
 const storeScore = (e) => {
   e.preventDefault();
   let existScore = JSON.parse(localStorage.getItem('scores'));
-  existScore = existScore == null? []: existScore;
+  existScore = existScore == null ? [] : existScore;
   const inputName = document.querySelector('.name').value;
   const inputScore = document.querySelector('.score').value;
 
   const newScore = new Score(inputName, inputScore);
 
-  if(inputName !== '' && inputScore !== ''){
+  if(inputName !== '' && inputScore !== '') {
     existScore.push(newScore);
     localStorage.setItem('scores', JSON.stringify(existScore));
     document.querySelector('.name').value = '';
     document.querySelector('.score').value = '';
     DispalyScore();
   }
-}
+};
 
-const DispalyScore = () => {
-  const existScore = JSON.parse(localStorage.getItem('scores'));
-  if(existScore !== null && existScore.length > 0) {
-    const scoreList = document.querySelector('.score-list');
-    const scoreItem = document.createElement('ul');
-
-    existScore.forEach((score, index) => {
-      const scoreListItem = document.createElement('li');
-      scoreListItem.innerHTML = score.name + " : " + score.score;
-      scoreItem.appendChild(scoreListItem);
-      scoreList.appendChild(scoreItem);
-    });
-  }
-} 
 
 document.querySelector('.submit-btn').addEventListener('click', storeScore);
 document.querySelector('.refresh-btn').addEventListener('click', DispalyScore);
