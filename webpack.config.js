c16f782eae0749072b80c805db6472cc5031a884
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-08-31 11:00:01 +0430

    Leaderboard Structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'LeaderBoard',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+};
